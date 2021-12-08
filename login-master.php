73145defd5fd734133d6dac5014300220b184e75
Alexanderh1988 <alex.utfsm@gmail.com>
2021-12-07 23:26:48 -0300

    commit 2
    no core

---
+++ login-master.php
@@ -0,0 +1,142 @@
+<?php
+
+$db = mysqli_connect('127.0.0.1', 'root', '', 'chs44206_dbhstech');
+
+if(mysqli_connect_error()) {
+    echo 'Database connection failed with following errors: '.mysqli_connect_error();
+    die();
+}
+
+session_start();
+
+$_SESSION['start'] = time(); // Taking now logged in time.
+// Ending a session in 30 minutes from the starting time.
+
+$id = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
+
+if (!empty($id)) {
+
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Login V12</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('images/img-01.jpg');">
+        <div class="wrap-login100 p-t-190 p-b-30">
+            <form class="login100-form validate-form" action="" method="POST">
+                <div class="login100-form-avatar">
+                    <!--                    <img src="images/avatar-01.jpg" alt="AVATAR">-->
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdmQEqIVtrKcBmDCxbRWkEARZ14r4ceFFxYYeJoWRyV17xi2lX-VKLyO3QB3d6mgAhNeY&usqp=CAU"
+                         alt="AVATAR">
+                </div>
+
+                <span class="login100-form-title p-t-20 p-b-45">
+						identifiquese
+					</span>
+
+ 				<?php
+                if ($_POST) {
+
+                $_SESSION['try']=1  ;
+
+                $getUsers = "SELECT * FROM `users` where full_name='" . $_POST['username'] . "'";
+
+                $results = $db->query($getUsers);
+                $row = $results->fetch_array(MYSQLI_NUM);
+
+                if (isset($row)) {
+
+                $clave = implode("", array_slice($row, 3, 1));
+                $id = implode("", array_slice($row, 0, 1));
+
+                if ($_POST['pass'] == $clave) {
+
+                $_SESSION["id"] = $id;
+                header('Location: index.php');
+                }
+                }
+                 }
+                 else {}
+                ?> <h4>Datos de ingreso incorrectos</h4>
+                <br> <br>
+
+                <div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+                    <input class="input100" type="text" name="username" placeholder="Usuario">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-user"></i>
+						</span>
+                </div>
+
+                <div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+                    <input class="input100" type="password" name="pass" placeholder="Clave">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-lock"></i>
+						</span>
+                </div>
+
+                <div class="container-login100-form-btn p-t-10">
+                    <button class="login100-form-btn" value="submit" type="submit">
+                        Entrar
+                    </button>
+                </div>
+
+        
+
+
+
+                <!--                <div class="text-center w-full p-t-25 p-b-230">-->
+                <!--                    <a href="#" class="txt1">-->
+                <!--                        Forgot Username / Password?-->
+                <!--                    </a>-->
+                <!--                </div>-->
+
+                <div class="text-center w-full">
+                    <!--						<a class="txt1" href="#">-->
+                    <!--							Create new account-->
+                    <!--							<i class="fa fa-long-arrow-right"></i>						-->
+                    <!--						</a>-->
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ users.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 05-12-2021 a las 16:27:50
+-- Versión del servidor: 5.7.34-cll-lve
+-- Versión de PHP: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `chs44206_dbhstech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `join_date` datetime NOT NULL,
+  `last_login` datetime NOT NULL,
+  `permissions` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `join_date`, `last_login`, `permissions`) VALUES
+(1, 'fulano', 'ejemplo@gmail.com', 'hstech', '2018-06-13 15:25:48', '2018-06-13 00:00:00', 'admin, editor'),
+;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
